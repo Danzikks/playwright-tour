bdac0dcf62b2976f81120efe6541c7e64432d961
daniilprusakov <prusakovdany@gmail.com>
2026-04-07 15:10:56 +0300

    wip

--- tests/my_project_site/test_login_page.py
+++ tests/my_project_site/test_login_page.py
@@ -1,5 +1,7 @@
 from time import sleep
 
+import pytest
+
 from pages.my_project_site.login_page import LoginPage
 from playwright.sync_api import expect
 
@@ -10,4 +12,18 @@ def test_login_page(page):
     login_page.fill_password_input('123456')
     login_page.click_submit_button()
     assert page.url == 'https://quick-tour.dprusakov.ru/'
+
+
+@pytest.mark.parametrize("login, password", [
+    ('test', '123456 '),
+    ('', '123456'),
+    ('123456', '123456'),
+])
+def test_negative_login_page(page, login, password):
+    page.goto('https://quick-tour.dprusakov.ru/login')
+    login_page = LoginPage(page)
+    login_page.fill_login_input(login)
+    login_page.fill_password_input(password)
+    login_page.click_submit_button()
+    assert page.url == 'https://quick-tour.dprusakov.ru/login'
 
